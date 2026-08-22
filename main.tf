766ff90b62711034ab7953ef74370fa611b04d6b
kushalrajak <kushal.rajak@outlook.com>
2026-08-22 21:23:57 +0530

    removed features

--- main.tf
+++ main.tf
@@ -11,11 +11,11 @@ provider "azurerm" {
   features {
 
   }
-  subscription_id                 = "a2b28c85-1948-4263-90ca-bade2bac4df4"
-  tenant_id                       = "30fe8ff1-adc6-444d-ba94-1238894df42c"
-  client_id                       = "394ba235-a73b-45e7-87d6-fd1481bc73e6"
-  client_secret                   = "65w8Q~AWLid4532QbBldDuje5_.iMFvf1JVLUaVB"
-  resource_provider_registrations = "none"
+  # subscription_id                 = "a2b28c85-1948-4263-90ca-bade2bac4df4"
+  # tenant_id                       = "30fe8ff1-adc6-444d-ba94-1238894df42c"
+  # client_id                       = "394ba235-a73b-45e7-87d6-fd1481bc73e6"
+  # client_secret                   = "65w8Q~AWLid4532QbBldDuje5_.iMFvf1JVLUaVB"
+  # resource_provider_registrations = "none"
 }
 
 variable "rg-name" {
