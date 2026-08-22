f3356aac9709659c155782014d23679dd9ada5e6
kushalrajak <40749720+kushalrajak@users.noreply.github.com>
2026-08-22 20:10:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,12 +13,12 @@ provider "azurerm" {
   }
   subscription_id                 = "a2b28c85-1948-4263-90ca-bade2bac4df4"
   tenant_id                       = "30fe8ff1-adc6-444d-ba94-1238894df42c"
-  client_id                       = "913bb14e-de4e-4741-9b48-3159123f87a3"
-  client_secret                   = "NBz8Q~wivUoeH2uY5oEGjqQ_mKUDbl5guJBAWdkl"
+  client_id                       = "394ba235-a73b-45e7-87d6-fd1481bc73e6"
+  client_secret                   = "65w8Q~AWLid4532QbBldDuje5_.iMFvf1JVLUaVB"
   resource_provider_registrations = "none"
 }
 
 variable "rg-name" {
   type    = string
-  default = "kml_rg_main-8917760fe90c4301"
+  default = "kml_rg_main-397925e0759649f7"
 }
