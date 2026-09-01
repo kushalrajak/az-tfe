6abcf3c966d91a90284e9daf733ca80acc375b64
kushalrajak <40749720+kushalrajak@users.noreply.github.com>
2026-09-01 21:31:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
   # tenant_id                       = "30fe8ff1-adc6-444d-ba94-1238894df42c"
   # client_id                       = "394ba235-a73b-45e7-87d6-fd1481bc73e6"
   # client_secret                   = "65w8Q~AWLid4532QbBldDuje5_.iMFvf1JVLUaVB"
-  # resource_provider_registrations = "none"
+  resource_provider_registrations = "none"
 }
 
 variable "rg-name" {
