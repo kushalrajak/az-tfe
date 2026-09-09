3d5426c9e68d8ec5cc0d6b478afef9a5638f74d1
kushalrajak <40749720+kushalrajak@users.noreply.github.com>
2026-09-09 12:52:03 +0530

    Update bastion.tf

--- bastion.tf
+++ bastion.tf
@@ -1,5 +1,6 @@
 resource "azurerm_subnet" "bastion-sub" {
   name                 = "AzureBastionSubnet"
+  resource_group_name  = var.rg-name
   address_prefixes     = ["10.0.1.0/24"]
   virtual_network_name = azurerm_virtual_network.dcvm-vnet.name
 }
